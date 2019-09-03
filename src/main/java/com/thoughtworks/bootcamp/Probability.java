3a63a36d11e4e12410fdbda7c453fdf4a806b15b
avanijayantkale <Avanikale20>
2019-09-03 22:58:32 +0530

    [Avani] Refactor. Functionality Probability of Event occure toughter

--- src/main/java/com/thoughtworks/bootcamp/Probability.java
+++ src/main/java/com/thoughtworks/bootcamp/Probability.java
@@ -1,15 +1,14 @@
 package com.thoughtworks.bootcamp;
 
 public class Probability {
-    private float firstEventForCoin;
-    private float secondEventForDice;
+    private final float firstEventForCoin;
+    private final float secondEventForDice;
 
     public Probability(float firstEventForCoin, float secondEventForDice) {
 
         this.firstEventForCoin = firstEventForCoin;
         this.secondEventForDice = secondEventForDice;
     }
-
 
     public float eventOccureTogether() {
         return firstEventForCoin * secondEventForDice;
